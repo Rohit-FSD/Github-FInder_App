9776b47bf8d9ace2c9d624f2e9ce4fe52d6678e0
Rohit Girotra <rohitgirotra555@gmail.com>
2020-08-15 14:29:23 +0530

    with build

---
+++ build/precache-manifest.aed2c701c74898102a0bec44421aee3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e5a205315ef222c3c005dd944540fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8d782c3e77e8ade7747",
+    "url": "/static/css/main.385bd870.chunk.css"
+  },
+  {
+    "revision": "b8b24ad08d87b83e6273",
+    "url": "/static/js/2.23e993b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.23e993b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d782c3e77e8ade7747",
+    "url": "/static/js/main.2889973b.chunk.js"
+  },
+  {
+    "revision": "25d48e391914dc08b45e",
+    "url": "/static/js/runtime-main.d89612ff.js"
+  }
+]);
